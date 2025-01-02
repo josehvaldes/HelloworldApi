fbacf35f9f88fb5813db781fb3c2399f6a4c66c2
josehvaldes <jmurguia@gmail.com>
2025-01-02 12:40:07 -0400

    Add basic project and config files.

---
+++ HelloWorldApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        public HealthCheckController() { }
+
+        [HttpGet("status")]
+        public IActionResult GetStatus() 
+        {
+            return Ok(new { status="Healthy" } );
+        }
+    }
+}
