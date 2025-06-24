f35e1bbe28a1d1a6cd4546c05e934bee3debeb50
josehvaldes <jmurguia@gmail.com>
2025-06-24 16:51:19 -0400

    Add Webhook endpoint to test eventgrid events

---
+++ HelloWorldApi/Controllers/EventgridEndpoint.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Azure.Messaging.EventGrid;
+using Microsoft.Extensions.Logging;
+using Azure.Messaging.EventGrid.SystemEvents;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HelloWorldApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventgridEndpoint : ControllerBase
+    {
+        private readonly ILogger<EventgridEndpoint> _logger;
+        public EventgridEndpoint(ILogger<EventgridEndpoint> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("events")]
+        public IActionResult Post([FromBody] JsonElement eventData)
+        {
+            _logger.LogInformation("Received event data: {EventData}", eventData);
+
+            BinaryData binaryData = BinaryData.FromObjectAsJson(eventData);
+
+            var eventGridEvents = EventGridEvent.ParseMany(binaryData);
+
+            foreach (var eventGridEvent in eventGridEvents)
+            {
+                if (eventGridEvent.EventType == "Microsoft.EventGrid.SubscriptionValidationEvent")
+                {
+                    // Handle subscription validation
+                    var data = eventGridEvent.Data.ToObjectFromJson<SubscriptionValidationEventData>();
+                    var response = new
+                    {
+                        validationResponse = data.ValidationCode
+                    };
+                    var json = Results.Json(response);
+                    return Ok(json);
+                }
+                else
+                {
+                    // Handle other events
+                    Console.WriteLine($"Received event: {eventGridEvent.EventType}");
+                    Console.WriteLine($"Subject: {eventGridEvent.Subject}");
+                    Console.WriteLine($"Data: {eventGridEvent.Data}");
+                }
+            }
+
+            return Ok();
+        }
+         
+    }
+}
